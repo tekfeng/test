2c75351fb1e10bbd47c17c1fde9ed240716992b4
Khoa Tran <khoatran@rubify.com>
2015-06-10 16:54:42 +0700

    upate incoming filter style

--- app/models/incoming.rb
+++ app/models/incoming.rb
@@ -1,6 +1,8 @@
 class Incoming < ActiveRecord::Base
   
   
+  SOURCE_INFO = ["info@amz", "sample" , "borneo@info"]
+  
   def self.generate_mockup_data
     name = ["josh", "minge", "nick", "moral", "kovac"]
     
@@ -10,7 +12,8 @@ class Incoming < ActiveRecord::Base
         customer_name: random_pick,
         email_address: "#{random_pick}@sample.com",
         title: "Interested in",
-        content: "Sed mollis ex in nulla euismod, vel consectetur sem malesuada. Curabitur gravida, ante et accumsan tempor, eros urna sollicitudin ligula, ac sollicitudin lorem mi feugiat sem. Nunc condimentum, erat sit amet pharetra sagittis, eros quam posuere tortor, eget facilisis nulla metus in lacus. Donec erat felis, tincidunt a velit at, rutrum condimentum libero. Sed eu est ligula. Integer viverra consectetur sem in tincidunt. Nunc blandit semper luctus."
+        content: "Sed mollis ex in nulla euismod, vel consectetur sem malesuada. Curabitur gravida, ante et accumsan tempor, eros urna sollicitudin ligula, ac sollicitudin lorem mi feugiat sem. Nunc condimentum, erat sit amet pharetra sagittis, eros quam posuere tortor, eget facilisis nulla metus in lacus. Donec erat felis, tincidunt a velit at, rutrum condimentum libero. Sed eu est ligula. Integer viverra consectetur sem in tincidunt. Nunc blandit semper luctus.",
+        source_info: Incoming::SOURCE_INFO.sample
       })
       
     end  
